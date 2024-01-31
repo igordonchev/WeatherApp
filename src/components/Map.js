008eef064edee21cecdc99f9aa2caae27778af42
IgorDonchev <igor.donchev@gmail.com>
2024-01-31 22:43:27 +0100

    map page added

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,29 +1,43 @@
-// Map.js
 import React, { useEffect, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const Map = ({ weatherData }) => {
+const Map = ({ topCities, onCityClick }) => {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    if (!mapRef.current && weatherData) {
-      const newMap = L.map('map').setView([weatherData.coord.lat, weatherData.coord.lon], 13);
+    console.log('Map component is rendered.');
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors',
-      }).addTo(newMap);
+    setTimeout(() => {
+      if (!mapRef.current && topCities && topCities.length > 0) {
+        console.log('Initializing map...');
+        const switzerlandCenter = [46.8182, 8.2275];
+        const newMap = L.map('map').setView(switzerlandCenter, 7);
 
-      L.marker([weatherData.coord.lat, weatherData.coord.lon])
-        .addTo(newMap)
-        .bindPopup(weatherData.name)
-        .openPopup();
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '© OpenStreetMap contributors',
+        }).addTo(newMap);
 
-      mapRef.current = newMap;  // Save the map instance to the ref
-    }
-  }, [weatherData]);
+        topCities.forEach((city) => {
+          if (city && city.coord && city.coord.lat && city.coord.lon) {
+            const marker = L.marker([city.coord.lat, city.coord.lon])
+              .addTo(newMap)
+              .bindPopup(city.name)
+              .on('click', () => onCityClick(city.coord));
+          }
+        });
 
-  return <div id="map" style={{ width: '300px', height: '300px', margin: 'auto' }}></div>;
+        mapRef.current = newMap;
+      }
+    }, 100); // Adjust the delay if needed
+  }, [topCities, onCityClick]);
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h2>Swiss Cities Map</h2>
+      <div id="map" style={{ display: 'inline-block', width: '300px', height: '300px' }}></div>
+    </div>
+  );
 };
 
 export default Map;
--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,57 +1,63 @@
-// MapPage.js
 import React, { useEffect, useState } from 'react';
 import api from '../components/api';
-import Map from '../components/Map';
-import '../styles/MapPage.css';
-
-// Import your components here
 import Layout from '../components/Layout';
 import Navigation from '../components/Navigation';
+import Map from '../components/Map';
 
 const MapPage = () => {
-  const [weatherData, setWeatherData] = useState(null);
+  const [topCities, setTopCities] = useState([]);
+  const [selectedCityWeather, setSelectedCityWeather] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchTopCities = async () => {
       try {
-        navigator.geolocation.getCurrentPosition(async (position) => {
-          const { latitude, longitude } = position.coords;
-
-          const response = await api.get('/weather', {
-            params: {
-              lat: latitude,
-              lon: longitude,
-            },
-          });
-
-          setWeatherData(response.data);
+        const response = await api.get('/group', {
+          params: {
+            id: '2657896,2657970,2657974,2657882,2658055,2657895,2657936,2657996,2658009,2657984,2660646,2659994,2659810,2659811,2659996,2659812,2657897',
+          },
         });
+        setTopCities(response.data.list);
+        console.log('Top Cities Data:', response.data.list);
       } catch (error) {
-        console.error('Error fetching weather data:', error);
+        console.error('Error fetching top cities:', error);
       }
     };
 
-    fetchData();
+    fetchTopCities();
   }, []);
 
+  const handleCityClick = async (coords) => {
+    try {
+      const response = await api.get('/weather', {
+        params: {
+          lat: coords.lat,
+          lon: coords.lon,
+        },
+      });
+
+      setSelectedCityWeather(response.data);
+    } catch (error) {
+      console.error('Error fetching city weather:', error);
+    }
+  };
+
   return (
-    <div>
+    <div style={{ textAlign: 'center', margin: 'auto' }}>
       <Layout>
         <Navigation />
       </Layout>
 
-      {weatherData && (
-        <div className="weather-info">
-          <Map weatherData={weatherData} />
+      <h1>Swiss Cities Weather</h1>
 
-          <div className="weather-details">
-            <h2>{weatherData.name}</h2>
-            <p>{weatherData.weather[0].description}</p>
-            <p>Temperature: {weatherData.main.temp}&deg;C</p>
-            <p>Humidity: {weatherData.main.humidity}%</p>
-            <p>Wind Speed: {weatherData.wind.speed} m/s</p>
-            {/* Add more weather information as needed */}
-          </div>
+      <Map topCities={topCities} onCityClick={handleCityClick} />
+
+      {selectedCityWeather && (
+        <div>
+          <h2>{selectedCityWeather.name} Weather</h2>
+          <p>Temperature: {selectedCityWeather.main.temp}°C</p>
+          <p>Humidity: {selectedCityWeather.main.humidity}%</p>
+          <p>Wind Speed: {selectedCityWeather.wind.speed} m/s</p>
+          {/* Add more weather information as needed */}
         </div>
       )}
     </div>
